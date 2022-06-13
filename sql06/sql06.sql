206e48c52e234522ef309fe64f4353d6c4dfd99a
Hector Marcos <hectormarcx@gmail.com>
2022-06-13 17:35:07 +0200

    sql09

---
+++ sql06/sql06.sql
@@ -0,0 +1,28 @@
+#1
+select * from movies;
+select director from movies;
+select title, director from movies;
+select title, year from movies;
+select * from movies;
+
+#2
+select * from movies where id = 6;
+select * from movies where year between 2000 and 2010
+select * from movies where year < 2000 or year > 2010;
+select * from movies order by year limit 5;
+
+#3
+SELECT * FROM movies where title like 'Toy%';
+SELECT * FROM movies where director = 'John Lasseter';
+SELECT * FROM movies where director != 'John Lasseter';
+SELECT * FROM movies where title like 'WALL-%';
+
+#4
+SELECT * FROM movies group by director;
+SELECT * FROM movies order by year desc limit 4;
+SELECT * FROM movies order by title limit 5;
+SELECT * FROM movies order by title limit 5 offset 5;
+
+#5
+select city, population from north_american_cities where country = 'Canada';
+select * from north_american_cities where country = 'United States' order by latitude desc;
